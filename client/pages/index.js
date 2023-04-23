392767705dff59cb3797b69be4df72ed8a19a4e8
Pranshu Kumar Agrawal <pranshukumar1999@gmail.com>
2023-04-23 13:49:28 +0530

    "Sidebar" component implemented

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,18 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Sidebar from '../components/Sidebar';
 
-const inter = Inter({ subsets: ['latin'] })
+const style = {
+  wrapper: `flex justify-center h-screen w-screen slect-none bg-[#15202b] text-white`,
+  content: `max-w-[1400px] w-2/3 flex justify-between`
+}
 
 export default function Home() {
   return (
-    <div>
-      <h2>Sidebar</h2>
+    <div className={style.wrapper}>
+      <div className={style.content}>
+      <Sidebar />
       <h2>Feed</h2>
       <h2>Widgets</h2>
+      </div>
     </div>
   )
 }
