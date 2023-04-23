6f95e715b437a619f2b95f541c7f705d97add45e
Pranshu Kumar Agrawal <pranshukumar1999@gmail.com>
2023-04-23 19:08:35 +0530

    TweetBox for Home implemented

--- client/components/home/Feed.js
+++ client/components/home/Feed.js
@@ -1,5 +1,6 @@
 import { BsStars } from 'react-icons/bs';
 import TweetBox from './TweetBox'
+import Post from '../Post';
 
 
 const style = {
@@ -7,6 +8,50 @@ const style = {
     header: `sticky top-0 bg-[#15202b] z-10 p-4 flex justify-between items-center`,
     headerTitle: `text-xl font-bold`,
   }
+
+const tweets = [
+    {
+    displayName: 'PkAgrawal',
+    userName: '0xnijn34nijn3nuu3nnv3o3n34nno4fonom4434fr4',
+    avatar: 'https://avatars.githubusercontent.com/u/43396684?v=4',
+    text: 'gm',
+    isProfileImageNft: false,
+    timestamp: '2023-06-01T12:00:00.000Z',
+    },
+    {
+    displayName: 'PkAgrawal',
+    userName: '0xnijn34nijn3nuu3nnv3o3n34nno4fonom4434fr4',
+    avatar: 'https://avatars.githubusercontent.com/u/43396684?v=4',
+    text: 'gm',
+    isProfileImageNft: false,
+    timestamp: '2023-06-01T12:00:00.000Z',
+    },
+    {
+    displayName: 'PkAgrawal',
+    userName: '0xnijn34nijn3nuu3nnv3o3n34nno4fonom4434fr4',
+    avatar: 'https://avatars.githubusercontent.com/u/43396684?v=4',
+    text: 'gm',
+    isProfileImageNft: false,
+    timestamp: '2023-06-01T12:00:00.000Z',
+    },
+    {
+    displayName: 'PkAgrawal',
+    userName: '0xnijn34nijn3nuu3nnv3o3n34nno4fonom4434fr4',
+    avatar: 'https://avatars.githubusercontent.com/u/43396684?v=4',
+    text: 'gm',
+    isProfileImageNft: false,
+    timestamp: '2023-06-01T12:00:00.000Z',
+    },
+    {
+    displayName: 'PkAgrawal',
+    userName: '0xnijn34nijn3nuu3nnv3o3n34nno4fonom4434fr4',
+    avatar: 'https://avatars.githubusercontent.com/u/43396684?v=4',
+    text: 'gm',
+    isProfileImageNft: false,
+    timestamp: '2023-06-01T12:00:00.000Z',
+    },
+
+]
 
 function Feed(){
     return(
@@ -18,6 +63,20 @@ function Feed(){
                 <BsStars />
             </div>
             <TweetBox />
+            {tweets.map((tweet,index) =>(
+                <Post
+                  key={index}
+                  displayName={tweet.displayName}
+                  userName={tweet.userName}
+                  avatar={tweet.avatar}
+                  text={tweet.text}
+                  isProfileImageNft={tweet.isProfileImageNft}
+                  timestamp={tweet.timestamp}
+
+                />
+            ))
+
+            }
 
         </div>
     )
