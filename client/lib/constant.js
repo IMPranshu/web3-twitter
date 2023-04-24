41b04c30b73f520efabe879e6b4b620301e63747
Pranshu Kumar Agrawal <pranshukumar1999@gmail.com>
2023-04-24 16:11:04 +0530

    contract connected with frontend

---
+++ client/lib/constant.js
@@ -0,0 +1,4 @@
+import contractArtifact from './ProfileImageNfts.json'
+
+export const contractAddress = '0x777D158121b78E603184aCB421DEE7567e183D0d'
+export const contractABI = contractArtifact.abi
